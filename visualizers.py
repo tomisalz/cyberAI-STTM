ab78bc5ec0b4e43f9fcf17e9b65de0826f08a058
tsaltsberg <tomsaltsberg@gmail.com>
2022-06-22 14:03:55 +0300

    small fix to visualizer

--- visualizers.py
+++ visualizers.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from script import GSDMM
 
 
-def create_word_cloud(gds: GSDMM):
+def create_word_cloud(word_dict):
 
 
     wordcloud = WordCloud(background_color='#fcf2ed',
                                 width=1800,
                                 height=700,
 
-                                colormap='flag').generate_from_frequencies(gds.clusters[1].nwz)
+                                colormap='flag').generate_from_frequencies(word_dict)
 
     # Print to screen
     fig, ax = plt.subplots(figsize=[20,10])
