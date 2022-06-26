90dd89f680ae3aba30edf4e539b618c417908dce
tsaltsberg <tomsaltsberg@gmail.com>
2022-06-26 22:48:41 +0300

    major performance improvements

--- detect_predators.py
+++ detect_predators.py
@@ -14,7 +14,8 @@ def mark_predators(clusters, fit_results, docs):
             if prediction in suspicious_clusts:
                 auths[doc.author][SUSP_MSG_COUNT] += 1
 
-    return [a for a in auths if auths[a][SUSP_MSG_COUNT] / auths[a][DOC_COUNT] > DEF_MSG_RATIO_THRESH]
+    # y_pred, y_true
+    return [int(auths[a][SUSP_MSG_COUNT] / auths[a][DOC_COUNT] > DEF_MSG_RATIO_THRESH) for a in auths ], [int(auths[a][0]) for a in auths]
 
 
 def detect_suspicious_clusters(clusters, thresh=DEF_CLUST_THRESH):
